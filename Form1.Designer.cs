6a071552321eb9fff35b1e4dabe98f5b6ab4261d
nAremon <104908611+nAremon@users.noreply.github.com>
2022-05-24 12:02:59 +0300

    lab11
    lab11

---
+++ Form1.Designer.cs
@@ -0,0 +1,76 @@
+﻿
+namespace WindowsFormsApp12
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 30;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(16, 527);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(44, 16);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "label1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1067, 554);
+            this.Controls.Add(this.label1);
+            this.DoubleBuffered = true;
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "Form1";
+            this.Text = "9-variant";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.Form1_Paint);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp12
+{
+    public partial class Form1 : Form
+    {
+        private float x1, x2, x3, y1, y2, y3, x4, y4, r1, r2, r3;
+        private double a1, a2, a3;
+        private SolidBrush b2 = new SolidBrush(Color.Red);
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            Graphics g = e.Graphics;
+            PointF p1 = new PointF(x1, y1);
+            PointF p2 = new PointF(x2, y2);
+            PointF p3 = new PointF(x3, y3);
+            PointF p4 = new PointF(x4, y4);
+            PointF[] points = {p1, p2, p3, p4};
+            g.FillClosedCurve(b2, points);
+
+            label1.Text = x2.ToString();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            a1 = 0;
+            a2 = 0.4;
+            a3 = 0.9;
+            r1 = 200;
+            r2 = 70;
+            r3 = 200;
+            x1 = ClientSize.Width / 2;
+            y1 = ClientSize.Height / 2;
+            x2 = x1 + (int)(r1 * Math.Cos(a1));
+            y2 = y1 - (int)(r1 * Math.Sin(a1));
+            x3 = x1 + (int)(r2 * Math.Cos(a2));
+            y3 = y1 - (int)(r2 * Math.Sin(a2));
+            x4 = x1 + (int)(r3 * Math.Cos(a3));
+            y4 = y1 - (int)(r3 * Math.Sin(a3));
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            a1 -= 0.1;
+            a2 -= 0.1;
+            a3 -= 0.1;
+            x2 = x1 + (int)(r1 * Math.Cos(a1));
+            y2 = y1 - (int)(r1 * Math.Sin(a1));
+            x3 = x1 + (int)(r2 * Math.Cos(a2));
+            y3 = y1 - (int)(r2 * Math.Sin(a2));
+            x4 = x1 + (int)(r3 * Math.Cos(a3));
+            y4 = y1 - (int)(r3 * Math.Sin(a3));
+            Invalidate();
+        }
+    }
+}
